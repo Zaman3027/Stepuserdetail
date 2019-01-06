e511a9f231a56f2878312cc1e16f553896dd2926
Mahafuz <zamanapp@outlook.com>
2019-01-07 02:37:34 +0530

    Updates

---
+++ precache-manifest.b760e8c80602a45cc6a9a0c154b2d4e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b27d32b2d584fd5bb31",
+    "url": "/Stepuserdetail/static/js/runtime~main.8b27d32b.js"
+  },
+  {
+    "revision": "4145c0d1f1812781b684",
+    "url": "/Stepuserdetail/static/js/main.4145c0d1.chunk.js"
+  },
+  {
+    "revision": "fba80f0aa3bf21e103ea",
+    "url": "/Stepuserdetail/static/js/1.fba80f0a.chunk.js"
+  },
+  {
+    "revision": "4145c0d1f1812781b684",
+    "url": "/Stepuserdetail/static/css/main.791422ff.chunk.css"
+  },
+  {
+    "revision": "078547a3bf7161541a0ddbb1c71d8ac7",
+    "url": "/Stepuserdetail/index.html"
+  }
+];
